7905098deb104fea3e9e4a858dc1ca95ce289863
plefevre <plefevre561@gmail.com>
2019-06-12 10:00:50 +0800

    added demo functions

--- demo.py
+++ demo.py
@@ -11,15 +11,19 @@ from epath import EPath
 
 
 
-def demo():
+def demo_basic():
+    """
+    demo showing basic usage and latex/csv manipulations
+
+    """
     image_fname = EPath("/tmp/image.png")
     # let's change the extension
     image_fname2 = image_fname.replace_suffix(".png22")
     # change dir
     image_fname3 = image_fname2.replace_parents("/someotherdir/a/b")
-    # print(image_fname)
-    # print(image_fname2)
-    # print(image_fname3)
+    print(image_fname)
+    print(image_fname2)
+    print(image_fname3)
 
 
     home = EPath("/tmp")
@@ -35,21 +39,49 @@ def demo():
     # fname.writedf_tocsv(df)
 
     # file name is
-    # print(fname)
-    # # stem is
-    # print(fname.stem())
-    # print(fname.suffix())
-    # print(fname.basename())
+    print(fname)
+    # stem is
+    print(fname.stem())
+    print(fname.suffix())
+    print(fname.basename())
 
     # add data after stem
     fname2 = fname.add_after_stem("01010jlkjlkj_LKlKlkj")
     df["count2"] = np.arange(10, 20)
-    # fname2.writedf_tocsv(df)
-    # fname2.write_tex(df.to_latex())
+    fname2.writedf_tocsv(df)
+    fname2.write_tex(df.to_latex())
 
     image_fname = EPath(HOME).join("Images/lena.png")
     image = image_fname.imread()
 
+
+def demo_path_manipulations():
+    file_name = EPath("/tmp/my_file.txt")
+
+    for i in range(10):
+        for j in range(10):
+            # some processing here...
+            p = file_name.add_after_stem("{}_{}".format(i, j))
+            print(p)
+
+    p_final = file_name.add_before_stem("awesome_experiment")
+    print(p_final)
+
+    # moving to another dir
+    p_final2 = p_final.replace_parents("/tmp/somedir/somedir2")
+    print(p_final2)
+
+    # joining
+    p_final3 = p_final.parent().join("../proc/omgdontsavehere")
+    print(p_final3)
+
+
+def demo_experimental_feature_setitem():
+    """
+    Experimental feature with __setitem__
+
+    """
+    image_fname = EPath("/tmp/image.png")
     npath = image_fname/"ff"/"jlkj"
 
     print("New path : ", npath)
@@ -61,9 +93,11 @@ def demo():
     print(npath[2])
     print(npath[3])
 
+    # this does not work... yet.
+    # print(npath[1:3])
 
 def main():
-    demo()
+    demo_path_manipulations()
 
 
 if __name__ == '__main__':
