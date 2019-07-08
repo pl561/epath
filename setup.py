70e70e093776c2e72aada37a7f2b5144462ed988
plefevre <plefevre561@gmail.com>
2019-07-08 18:07:48 +0800

    added setup and requirements files to install with pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="epath",
+    version="1.0.0",
+    author_email="plefevre561@gmail.com",
+    description="This module contains tools for manipulating Linux paths.",
+    license="MIT License",
+)
